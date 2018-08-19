3359c7191d71b1918aa4daf92c4a5ffef1ee422a
MariannaSorrentino <40571749+MariannaSorrentino@users.noreply.github.com>
2018-08-19 09:08:51 +0200

    modificato contenuto schema

--- server/models/model/contenuto.js
+++ server/models/model/contenuto.js
@@ -2,150 +2,47 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 
 var ContenutoSchema = new mongoose.Schema({
-  id:{
-    type: Number,
-    trim: true,
-    minlength: 1,
-    unique: true
-  },
-  sezione: {
-    type: String,
-    require: true
-  },
-  titolo:{
-      type:String,
-      required:false,
-      minlength:1
-  },
-  corpo:{
-    type:String,
-    required:false,
-    minlength:1
-  },
-  lingua:{
-    type:String,
-    required:false,
-    minlength:1
-  },
-  attivo:{
-    type:Boolean,
-    required:true
-  },
-  tsCreazione:{
-    type:Date,
-    required:false,
-    minlength:1
-  },
-  autore:{
-    type:String,
-    required:false,
-    minlength:1   
-  },
-  commento:{
-    type:String,
-    required:false,
-    minlength:1
-  },
-  multimedia:{
-    type:{
-        id:{
-          type:Number,
-          required:true,
-          unique:true
-        },  
-        tipo:{
-          type:String,
-          required:true,
-          minlength:3
-        },
-        titolo:{
-          type:String,
-          required:true,
-          minlength:1
-        },
-        url:{
-          type:String,
-          required:true,
-          minlength:1
-        },
-        urls:{
-          type:[String],
-          required:false
-        },
-        alt:{
-          type:String,
-          required:false
-        },
-        data:{
-          type:String,
-          required:true,
-          default: Date.now
-        },
-        luogo:{
-          type:String,
-          required:false
-        },
-        autore:{
-          type:String,
-          required:false
-        }
-    },
-    required:true,
-  },
-  album:{
-  type:[{
-    id:{
-      type:Number,
-      required:true,
-      unique:false
-    },  
-    tipo:{
-      type:String,
-      required:false,
-      minlength:3
-    },
-    titolo:{
-      type:String,
-      required:false,
-      minlength:1
-    },
-    url:{
-      type:String,
-      required:false,
-      minlength:1
-    },
-    urls:{
-      type:[String],
-      required:false
-    },
-    alt:{
-      type:String,
-      required:false
-    },
-    data:{
-      type:String,
-      required:false,
-      default: Date.now
-    },
-    luogo:{
-      type:String,
-      required:false
-    },
-    autore:{
-      type:String,
-      required:false
-    }
-  }],
-  required:false
-  },
-  campi:{
-  type:[String],
-  required:false
-  },
-  links:{
-  type:[String],
-  required:false
-  }
+  id:{type: Number, trim: true, minlength: 1, unique: true },
+
+  sezione: {type: String, require: true },
+
+  titolo:{type:String, required:false, minlength:1 },
+
+  corpo:{type:String, required:false, minlength:1 },
+
+  lingua:{type:String, required:false, minlength:1 },
+  
+  attivo:{type:Boolean, required:true },
+  
+  tsCreazione:{type:Date, required:false, minlength:1 },
+
+  autore:{type:String, required:false, minlength:1 },
+
+  commento:{type:String, required:false, minlength:1 },
+
+  multimedia:{type:{id:{type:Number, required:false },  
+                    tipo:{type:String, required: false },
+                    titolo:{type:String, required:false },
+                    url:{type:String, required:false },
+                    urls:{type:[String], required:false },
+                    alt:{type:String, required:false },
+                    data:{type:String, required:false },
+                    luogo:{type:String, required:false },
+                    autore:{type:String, required:false }}, required:true},
+
+  album:{type:[{id:{type:Number, required:false},  
+                tipo:{type:String, required:falsec},
+                titolo:{type:String, required:false },
+                url:{type:String, required:false },
+                urls:{type:[String], required:false},
+                alt:{type:String, required:false },
+                data:{type:String, required:false },
+                luogo:{type:String, required:false },
+                autore:{type:String, required:false }}], required:false},
+  
+  campi:{type:[String], required:false },
+  
+  links:{type:[String], required:false }
 });
 
 ContenutoSchema.methods.toJSON = function () {
