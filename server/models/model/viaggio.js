4e5e580c2a927734dd85a5abb36ede85ec4d7423
MariannaSorrentino <40571749+MariannaSorrentino@users.noreply.github.com>
2018-08-19 15:55:07 +0200

    modificato schema viaggio

--- server/models/model/viaggio.js
+++ server/models/model/viaggio.js
@@ -3,19 +3,19 @@ const _ = require('lodash');
 
 var ViaggioSchema = new mongoose.Schema({
 
-  id: {type: Number, trim: true, minlength: 1,unique: true},
+  id: {type: Number, trim: true, minlength: 1, unique: true},
 
-  nome: {type: String,require: true},
+  nome: {type: String, require: true},
 
-  titolo: {type:String,required:false, minlength:1},
+  titolo: {type:String, required:false, minlength:1},
 
-  sottotitolo: {type:String,required:false},
+  sottotitolo: {type:String, required:false},
 
-  sommario: {type:String,required:false},
+  sommario: {type:String, required:false},
 
-  descrizione: {type:String,required:false, minlength:1},
+  descrizione: {type:String, required:false, minlength:1},
 
-  destinazione: {type:String,required:true},
+  destinazione: {type:String, required:false},
 
   tag: {type:String, required:false},
 
@@ -23,7 +23,7 @@ var ViaggioSchema = new mongoose.Schema({
 
   partenza: {type:String, required:false, minlength:1},
 
-  ritorno: {type:String,required:false, minlength:1},
+  ritorno: {type:String, required:false, minlength:1},
 
   multimedia: {type:{id:{type:Number,required:true,unique:true},  
                      tipo:{type:String,required:true,minlength:3},
@@ -76,6 +76,7 @@ var ViaggioSchema = new mongoose.Schema({
                               data:{type:String,required:true, default: Date.now},
                               luogo:{type:String,required:false},
                               autore:{type:String, required:false}}],required:false},
+
                  tappe:{type:[{
                        id:{type:Number, required:false},
                        titolo:{type:String, required:false},
@@ -88,13 +89,13 @@ var ViaggioSchema = new mongoose.Schema({
                        ritorno:{type:String, required:false},
                        multimedia:{type:{id:{type:Number,required:true,unique:true},  
                                          tipo:{type:String,required:true,minlength:3},
-                                       titolo:{type:String,required:true,minlength:1},
-                                       url:{type:String,required:true,minlength:1},
-                                       urls:{type:[String],required:false},
-                                       alt:{type:String,required:false},
-                                       data:{type:String,required:true,default: Date.now},
-                                       luogo:{type:String,required:false},
-                                       autore:{type:String,required:false}}, required:false},
+                                         titolo:{type:String,required:true,minlength:1},
+                                         url:{type:String,required:true,minlength:1},
+                                         urls:{type:[String],required:false},
+                                         alt:{type:String,required:false},
+                                         data:{type:String,required:true,default: Date.now},
+                                         luogo:{type:String,required:false},
+                                         autore:{type:String,required:false}}, required:false},
                        album:{type:[{id:{type:Number,required:true,unique:true},  
                                      tipo:{type:String,required:true, minlength:3},
                                      titolo:{type:String,required:true, minlength:1},
